e79c946ba986158a5bd00f4df073300d65515f09
francesca-campana <franx3rox@gmail.com>
2020-06-16 19:09:49 +0200

    js full

--- js/script.js
+++ js/script.js
@@ -1,26 +1,51 @@
 $(document).ready(function(){
 
-
   $('.plane').click(function(){
+    $(this).addClass('active');
+    $('.phone').removeClass('active');
     var textAdd = $('#text-add').val();
     console.log(textAdd);
     var template = $('.template > .message-template').clone();
     console.log(template);
     template.children('span').text(textAdd);
-    template.addClass('msg-right');
+    template.addClass('msg-right green');
     var messageChat = $('.js-chat').append(template);
+    $('#text-add').val('');
+
+    setTimeout(function (){
+      var template = $('.template > .message-template').clone();
+      template.children('span').text('Ciao');
+      template.addClass('msg-left white');
+      var messageChat = $('.js-chat').append(template);
 
 
-
+    },3000);
 
   });
-  setTimeout(function (){
-    var template = $('.template > .message-template').clone();
-    template.children('span').text('Ciao');
-    template.addClass('msg-left, white');
-    var messageChat = $('.js-chat').append(template);
+  
+  $('#text-add').keypress(function(event){
+    if (event.which === 13) {
+      var textAdd = $('#text-add').val();
+      console.log(textAdd);
+      var template = $('.template > .message-template').clone();
+      console.log(template);
+      template.children('span').text(textAdd);
+      template.addClass('msg-right green');
+      var messageChat = $('.js-chat').append(template);
+      $('#text-add').val('');
+
+      setTimeout(function (){
+       var template = $('.template > .message-template').clone();
+       template.children('span').text('Ciao');
+       template.addClass('msg-left white');
+       var messageChat = $('.js-chat').append(template);
 
 
-  },5000);
+      },5000);
+
+    }
+
+  });
+
 
 });
