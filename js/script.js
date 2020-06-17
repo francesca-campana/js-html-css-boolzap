382a079a9d34aae52a3449e3245d56c87a2e4419
francesca-campana <franx3rox@gmail.com>
2020-06-17 13:51:38 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,75 +1,63 @@
-
- var d = new Date();
- var minutes = d.getMinutes();
- var hours = d.getHours();
- console.log(hours);
- console.log(minutes);
-
 $(document).ready(function(){
 
-  $('.plane').click(function(){
-    $(this).toggle();
-    $('.phone').toggle();
-    var textAdd = $('#text-add').val();
-    console.log(textAdd);
-    var timeAdd = $ ('#time').text(hours, minutes);
-    console.log(timeAdd);
-    var template = $('.template > .message-template').clone();
-    console.log(template);
+
+  $('.send-message').click(function(){
+    $('.plane').toggleClass('active');
+    $('.phone').toggleClass('active');
 
 
-    template.children('span').text(textAdd);
-    template.children('#time').text(timeAdd);
 
-
-    // $('p').text(hours);
-    // console.log(hours);
-
-    template.children('#time').text(timeAdd);
-    template.addClass('msg-right green');
-
-    var messageChat = $('.js-chat').append(template);
-    $('#text-add').val('');
+    // $('.send-message').removeClass('active');
+    sendMessage();
 
     setTimeout(function (){
-      var template = $('.template > .message-template').clone();
-      template.children('span').text('Ciao');
-
-      //template.children('p').text(timeAdd);
-      template.addClass('msg-left white');
-      var messageChat = $('.js-chat').append(template);
-
-
+      receivedMessage();
     },3000);
 
   });
 
   $('#text-add').keypress(function(event){
     if (event.which === 13) {
-      var textAdd = $('#text-add').val();
-      console.log(textAdd);
-      var template = $('.template > .message-template').clone();
-      console.log(template);
-      var timeAdd = $ ('#time').text(hours);
-      template.children('span').text(textAdd);
-      template.children('#time').text(timeAdd);
-      template.addClass('msg-right green');
-      var messageChat = $('.js-chat').append(template);
-      $('#text-add').val('');
+      sendMessage();
 
       setTimeout(function (){
-       var template = $('.template > .message-template').clone();
-       template.children('span').text('Ciao');
-
-       template.addClass('msg-left white');
-       var messageChat = $('.js-chat').append(template);
-
-
+        receivedMessage();
      },3000);
 
     }
 
   });
 
+  function dateNow(){
+    var d = new Date();
+    var minutes = d.getMinutes();
+    var hours = d.getHours();
 
-});
+    if (hours < 10){
+      return '0' + hours;
+
+    }else if (minutes < 10) {
+      return '0' + minutes;
+    }
+    var timeIs = hours + ':' + minutes;
+    var timeAdd = $ ('.time').text(timeIs);
+  }
+
+  function receivedMessage() {
+    var template = $('.template > .message-template').clone();
+    template.children('.text').text('Ciao');
+
+    template.addClass('msg-received');
+    var messageChat = $('.js-chat').append(template);
+  }
+  function sendMessage(){
+    var textAdd = $('#text-add').val();
+    var template = $('.template > .message-template').clone();
+    var timeAdd = dateNow();
+    template.children('.text').text(textAdd);
+    template.children('.time').text(timeAdd);
+    template.addClass('msg-send');
+    var messageChat = $('.js-chat').append(template);
+    $('#text-add').val('');
+  }
+ });
